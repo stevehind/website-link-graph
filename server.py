cc95145ea2b7f45853c7217ad2f8acc210c67fef
stevehind <steve.hind@gmail.com>
2020-09-04 18:26:57 -0700

    Prototype running. Can't get image to reliably render via API, but renders in tests. Need to add in ability to map network N layers deep.

---
+++ server.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify, Response, render_template
+import json
+import io
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__),'./src'))
+from utils import ScrapedWebsite, WebsiteGraph
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET'])
+def hello_world():
+    return "Hello, world!"
+
+@app.route('/api/v1/submit_url', methods = ['POST'])
+def handle_url():
+    request_data = json.loads(request.data)
+
+    url = request_data['url']
+    print('Request url is: '+ url)
+
+    graph = WebsiteGraph(url, 1)
+    graph_image = graph.display_graph
+
+    return render_template('index.html', url = url)
+
+if (__name__ == '__main__'):
+    app.run(host='0.0.0.0', debug = True)
